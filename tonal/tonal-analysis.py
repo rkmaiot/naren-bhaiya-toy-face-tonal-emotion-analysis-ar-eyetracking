064604d05875be5d5cecffe01d47389538cd25f6
rkmapps <50436757+rkmapps@users.noreply.github.com>
2021-01-19 17:57:59 +0530

    update tonal model.

--- tonal/tonal-analysis.py
+++ tonal/tonal-analysis.py
@@ -14,49 +14,50 @@
 #	Angry: 0.001
 #	Fear: 0.000
 
+import Vokaturi
 import sys
 import scipy.io.wavfile
 
-sys.path.append("C:\\Users\\webdeveloper\\Downloads\\OpenVokaturi-3-4\\OpenVokaturi-3-4\\api")
-import Vokaturi
+sys.path.append(
+    "C:\\Users\\webdeveloper\\Downloads\\OpenVokaturi-3-4\\OpenVokaturi-3-4\\api")
 
-print ("Loading library...")
-Vokaturi.load("C:\\Users\\webdeveloper\\Downloads\\OpenVokaturi-3-4\\OpenVokaturi-3-4\\lib\\open\\win\\OpenVokaturi-3-4-win64.dll")
-print ("Analyzed by: %s" % Vokaturi.versionAndLicense())
+print("Loading library...")
+Vokaturi.load(
+    "C:\\Users\\webdeveloper\\Downloads\\OpenVokaturi-3-4\\OpenVokaturi-3-4\\lib\\open\\win\\OpenVokaturi-3-4-win64.dll")
+print("Analyzed by: %s" % Vokaturi.versionAndLicense())
 
-print ("Reading sound file...")
+print("Reading sound file...")
 file_name = sys.argv[1]
-# file_name = "C:\\Users\\webdeveloper\\Downloads\\OpenVokaturi-3-4\\OpenVokaturi-3-4\\examples\\6.wav"
 (sample_rate, samples) = scipy.io.wavfile.read(file_name)
-print ("   sample rate %.3f Hz" % sample_rate)
+print("   sample rate %.3f Hz" % sample_rate)
 
-print ("Allocating Vokaturi sample array...")
+print("Allocating Vokaturi sample array...")
 buffer_length = len(samples)
-print ("   %d samples, %d channels" % (buffer_length, samples.ndim))
+print("   %d samples, %d channels" % (buffer_length, samples.ndim))
 c_buffer = Vokaturi.SampleArrayC(buffer_length)
 if samples.ndim == 1:  # mono
-	c_buffer[:] = samples[:] / 32768.0
+    c_buffer[:] = samples[:] / 32768.0
 else:  # stereo
-	c_buffer[:] = 0.5*(samples[:,0]+0.0+samples[:,1]) / 32768.0
+    c_buffer[:] = 0.5*(samples[:, 0]+0.0+samples[:, 1]) / 32768.0
 
-print ("Creating VokaturiVoice...")
-voice = Vokaturi.Voice (sample_rate, buffer_length)
+print("Creating VokaturiVoice...")
+voice = Vokaturi.Voice(sample_rate, buffer_length)
 
-print ("Filling VokaturiVoice with samples...")
+print("Filling VokaturiVoice with samples...")
 voice.fill(buffer_length, c_buffer)
 
-print ("Extracting emotions from VokaturiVoice...")
+print("Extracting emotions from VokaturiVoice...")
 quality = Vokaturi.Quality()
 emotionProbabilities = Vokaturi.EmotionProbabilities()
 voice.extract(quality, emotionProbabilities)
 
 if quality.valid:
-	print ("Neutral: %.3f" % emotionProbabilities.neutrality)
-	print ("Happy: %.3f" % emotionProbabilities.happiness)
-	print ("Sad: %.3f" % emotionProbabilities.sadness)
-	print ("Angry: %.3f" % emotionProbabilities.anger)
-	print ("Fear: %.3f" % emotionProbabilities.fear)
+    print("Neutral: %.3f" % emotionProbabilities.neutrality)
+    print("Happy: %.3f" % emotionProbabilities.happiness)
+    print("Sad: %.3f" % emotionProbabilities.sadness)
+    print("Angry: %.3f" % emotionProbabilities.anger)
+    print("Fear: %.3f" % emotionProbabilities.fear)
 else:
-	print ("Not enough sonorancy to determine emotions")
+    print("Not enough sonorancy to determine emotions")
 
 voice.destroy()
